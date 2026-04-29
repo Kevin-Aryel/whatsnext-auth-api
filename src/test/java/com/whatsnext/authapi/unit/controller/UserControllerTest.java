88585bfdcfe3679906cda4b4683bea42f7e65517
Kevin Aryel <kevin.aryel9@gmail.com>
2026-04-28 21:31:07 -0300

    fix(test): wire TestNG Surefire provider and fix H2 TCP startup ordering

--- src/test/java/com/whatsnext/authapi/unit/controller/UserControllerTest.java
+++ src/test/java/com/whatsnext/authapi/unit/controller/UserControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -21,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-class UserControllerTest {
+class UserControllerTest extends AbstractTestNGSpringContextTests {
 
     @Autowired MockMvc mockMvc;
     @Autowired ObjectMapper objectMapper;
